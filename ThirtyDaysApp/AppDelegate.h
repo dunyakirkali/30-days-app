146f68e67dfee23ef952b63ae24b7573fed7edfe
Dunya Kirkali <dunyakirkali@gmail.com>
2013-07-09 11:06:47 +0200

    Initial Commit

---
+++ ThirtyDaysApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ThirtyDaysApp
+//
+//  Created by Dunya Kirkali on 7/9/13.
+//  Copyright (c) 2013 Dunya Kirkali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
